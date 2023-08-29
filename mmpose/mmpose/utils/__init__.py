d38b23932deac1871b62704d1eecd0c8fe1d3b8a
Joel Navez CT <110367357+Navezjt@users.noreply.github.com>
2023-08-28 20:05:18 -0500

    Add files via upload

--- mmpose/mmpose/utils/__init__.py
+++ mmpose/mmpose/utils/__init__.py
@@ -1,13 +1,10 @@
 # Copyright (c) OpenMMLab. All rights reserved.
-from .camera import SimpleCamera, SimpleCameraTorch
-from .collect_env import collect_env
-from .config_utils import adapt_mmdet_pipeline
-from .logger import get_root_logger
-from .setup_env import register_all_modules, setup_multi_processes
-from .timer import StopWatch
+from .check_and_update_config import check_and_update_config
+from .ckpt_convert import pvt_convert
+from .rtmcc_block import RTMCCBlock, rope
+from .transformer import PatchEmbed, nchw_to_nlc, nlc_to_nchw
 
 __all__ = [
-    'get_root_logger', 'collect_env', 'StopWatch', 'setup_multi_processes',
-    'register_all_modules', 'SimpleCamera', 'SimpleCameraTorch',
-    'adapt_mmdet_pipeline'
+    'PatchEmbed', 'nchw_to_nlc', 'nlc_to_nchw', 'pvt_convert', 'RTMCCBlock',
+    'rope', 'check_and_update_config'
 ]
